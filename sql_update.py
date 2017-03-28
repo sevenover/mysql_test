664a4acdf195ad1e64093ba993117eb36d4b3886
sevenover <706390507@qq.com>
2017-03-28 16:33:53 +0800

    第二次修改
    删除数据

--- sql_update.py
+++ sql_update.py
@@ -4,16 +4,16 @@ import pymysql
 conn = pymysql.connect(user='root',host='localhost', db='school')
 cur = conn.cursor()
 # 修改数据
-sql='update school set name=%s where id=4'#使用sql语句,无论我们要插入的数据是什么类型,占位符都可以用%s  
-params=('sb1')  
-cur.execute(sql,params)  
-conn.commit()
-
-# 删除行
-# sql='delete from school where id=%s'#使用sql语句,无论我们要插入的数据是什么类型,占位符都可以用%s  
-# params=(2)  
+# sql='update school set name=%s where id=4'#使用sql语句,无论我们要插入的数据是什么类型,占位符都可以用%s  
+# params=('sb1')  
 # cur.execute(sql,params)  
 # conn.commit()
+
+# 删除行
+sql='delete from school where id=%s'#使用sql语句,无论我们要插入的数据是什么类型,占位符都可以用%s  
+params=(2)  
+cur.execute(sql,params)  
+conn.commit()
 
 cur.close()    
 conn.close()
